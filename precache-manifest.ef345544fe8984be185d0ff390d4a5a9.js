b4ffe7198028f6004e4fd2e8eced1d38f8e30bba
tonydevpro <antonio.1358@hotmail.com>
2025-02-13 14:55:40 -0300

    Updates

---
+++ precache-manifest.ef345544fe8984be185d0ff390d4a5a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f17cb731b3098f054f7b9bc22481ba",
+    "url": "/mudando-temas/index.html"
+  },
+  {
+    "revision": "0d04236324e562fcb027",
+    "url": "/mudando-temas/static/css/main.dfa27435.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/mudando-temas/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "8e3a127b3eb410b6d841",
+    "url": "/mudando-temas/static/js/3.445915d0.chunk.js"
+  },
+  {
+    "revision": "0d04236324e562fcb027",
+    "url": "/mudando-temas/static/js/main.49993785.chunk.js"
+  },
+  {
+    "revision": "381c24c8108697a6763d",
+    "url": "/mudando-temas/static/js/runtime~main.3ae38f01.js"
+  }
+]);
